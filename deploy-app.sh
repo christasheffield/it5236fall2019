684c11c04dad2cbe0144599b0383c72218190150
christasheffield <46790867+christasheffield@users.noreply.github.com>
2019-10-23 11:13:57 -0400

    Update deploy-app.sh

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-git pull && docker build --no-cache -f "docker/Dockerfile" -t doit-app . && docker stop -t 0 doit-app-container && docker rm doit-app-container && docker run -d --name doit-app-container -p 80:80 doit-app
+git pull && docker build --no-cache -t doit-app . && docker stop -t 0 doit-app-container && docker rm doit-app-container && docker run -d --name doit-app-container -p 80:80 doit-app
